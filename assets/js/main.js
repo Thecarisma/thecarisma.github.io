06c53c9014b1fd1dfa6fba55200f37087465d6d0
Adewale Azeez <azeezadewale98@gmail.com>
2020-01-18 22:46:55 +0100

    update website

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,15 @@
 
+openHome();
 
 function openHome() {
-    
+    openDivById("Home");
+}
+
+function openDivById(id) {
+    var el = document.getElementById('main');
+    var arr = el.childNodes;
+    for (var a = 0; a < arr.length; a++) { 
+        arr[a].display = "none"; 
+    }
+    document.getElementById(id).display = "block";
 }
