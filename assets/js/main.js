4744717c5e323ea430eda6382f34c7a64a609e7e
Adewale Azeez <azeezadewale98@gmail.com>
2020-01-18 23:06:50 +0100

    update website

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,21 @@ openHome();
 
 function openHome() {
     openDivById("Home");
+    setIconColor("fa-home");
+}
+
+function openProjects() {
+    //openDivById("Projects");
+    setIconColor("fa-align-center");
+}
+
+function openBlog() {
+    setIconColor("fa-edit");
+}
+
+function openContact() {
+    //openDivById("Contact");
+    setIconColor("fa-paper-plane");
 }
 
 function openDivById(id) {
@@ -13,4 +28,12 @@ function openDivById(id) {
     }
     document.getElementById(id).display = "block";
     document.getElementById(id).display = "block";
+}
+
+function setIconColor(id) {
+    var iconIds = ["fa-home", "fa-align-center", "fa-edit", "fa-paper-plane"];
+    for (var iconId of iconIds) {
+        document.getElementById(iconId).style.color = "white";
+    }
+    document.getElementById(id).style.color = "green"
 }
