e633fd9a7e8161274e5fac4574299c44ac6b6155
Adewale Azeez <azeezadewale98@gmail.com>
2020-01-19 07:53:03 +0100

    update website

--- assets/js/variables.js
+++ assets/js/variables.js
@@ -1,5 +1,22 @@
 
-const BlogPostFeatured = [
+const AllBlogPosts = [
+    {
+        title: "Managing Windows PFX certificates through PowerShell",
+        link: "https://dev.to/iamthecarisma/managing-windows-pfx-certificates-through-powershell-3pj",
+        site: "dev.to",
+        tags: "#powershell #windows #system #cli",
+        date: "Jan 17 2020"
+    },
+    {
+        title: "Enabling and disabling USB Disk Drive using C#",
+        link: "https://dev.to/iamthecarisma/enabling-and-disabling-usb-disk-drive-using-c-4dg5",
+        site: "dev.to",
+        tags: "#csharp #dotnet #windows #devmgmt",
+        date: "Dec 4 2019"
+    }
+]
+
+const AllProjects = [
     {
         title: "Managing Windows PFX certificates through PowerShell",
         link: "https://dev.to/iamthecarisma/managing-windows-pfx-certificates-through-powershell-3pj",
