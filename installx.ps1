aa43dcd6ce3396db9afeb341108c2fcd78b51201
Adewale Azeez <azeezadewale98@gmail.com>
2020-02-29 12:12:19 +0100

    add the cronux installx script

---
+++ installx.ps1
@@ -0,0 +1,86 @@
+#Requires -RunAsAdministrator
+#Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('http://localhost:8020/remoterun.ps1'))
+
+$AppName = "Cronux"
+$Version = "2.0"
+$AppArchiveUrl = "https://github.com/Thecarisma/Cronux/archive/master.zip"
+$InstallationPath = $env:ProgramData + "\$AppName\"
+$PathEnvironment = "User"
+$BeforeScript = ""
+$AfterScript = "
+    cp ./Cronux-master/commands/extractx.ps1 $InstallationPath
+    cp ./Cronux-master/commands/buildcronux.ps1 $InstallationPath
+    cp ./Cronux-master/commands/batforps.ps1 $InstallationPath
+    cp ./Cronux-master/commands/wrapcommand.ps1 $InstallationPath
+    cp ./Cronux-master/*.bat $InstallationPath
+    cp ./Cronux-master/*.sh $InstallationPath
+    cp ./Cronux-master/LICENSE $InstallationPath
+    powershell -noprofile -executionpolicy bypass -file ./extractx.ps1 ./ExportList.txt
+    powershell -noprofile -executionpolicy bypass -file ./buildcronux.ps1  ./ ./
+    Remove-Item -path Cronux-master -recurse
+"
+
+$AddPath = $true
+
+$Path = [Environment]::GetEnvironmentVariable('Path', "$PathEnvironment")
+$TEMP = Join-Path $env:SystemDrive "temp\installx\$AppName"
+
+Function Check-Create-Directory {
+    Param([string]$folder)
+    
+    If ( -not [System.IO.Directory]::Exists($folder)) {
+        [System.IO.Directory]::CreateDirectory($folder) > $null
+        If ( -not $?) {
+            Return
+        }
+    }
+}
+
+Function Download-App-Archive {
+    Invoke-WebRequest $AppArchiveUrl -OutFile "$TEMP\installx_package_.zip"
+}
+
+Function Extract-App-Archive {
+    Param([string]$archive_path, [string]$extact_folder)
+    
+    If ( -not [System.IO.File]::Exists($archive_path)) {
+        Write-Error "Cannot find the downloaded archive, stopping installation"
+        Return
+    }
+    Check-Create-Directory $extact_folder
+    Expand-Archive $archive_path -DestinationPath $extact_folder -Force
+}
+
+Function Add-Folder-To-Path {
+    Param([string]$folder)
+    
+    $NewPath = ""    
+    ForEach ($_path in $Path.Split(";")) {
+        If ($_path -ne $InstallationPath -and $_path -ne "") {
+            $NewPath += "$_path;"
+        }
+    }
+    [Environment]::SetEnvironmentVariable("Path", "$NewPath$folder", "$PathEnvironment")
+}
+
+"Preparing to install $AppName $Version"
+Check-Create-Directory $TEMP
+If ($BeforeScript -ne "") {
+    "Executing the BeforeScript..."
+    iex "$BeforeScript"
+}
+"Downloading the program archive..."
+Download-App-Archive
+Check-Create-Directory $InstallationPath
+"Installing $AppName $Version in $InstallationPath"
+Extract-App-Archive "$TEMP\installx_package_.zip" "$InstallationPath"
+If ($AddPath -eq $true) { 
+    "Adding $InstallationPath to $PathEnvironment Path variable"
+    Add-Folder-To-Path "$InstallationPath" 
+}
+Set-Location -Path $InstallationPath
+If ($AfterScript -ne "") {
+    "Executing the AfterScript..."
+    iex "$AfterScript"
+}
+"Installtion completes."
