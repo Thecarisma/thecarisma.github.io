b84a2b46b771f43e07c35a29c40c4c4a8fe1c74f
Adewale Azeez <azeezadewale98@gmail.com>
2021-04-19 10:35:23 +0100

    change website

---
+++ old/assets/js/variables.js
@@ -0,0 +1,466 @@
+
+const AllBlogPosts = [
+    {
+        title: "Hosting a maven repository on Github: site-maven-plugin",
+        link: "https://dev.to/iamthecarisma/hosting-a-maven-repository-on-github-site-maven-plugin-9ch",
+        site: "dev.to",
+        tags: "Github, Java, Maven, Android",
+        date: "Jan 27, 2020"
+    },
+    {
+        title: "Managing Windows PFX certificates through PowerShell",
+        link: "https://dev.to/iamthecarisma/managing-windows-pfx-certificates-through-powershell-3pj",
+        site: "dev.to",
+        tags: "Powershell, Windows, System, CLI",
+        date: "Jan 17, 2020"
+    },
+    {
+        title: "Enabling and disabling USB Disk Drive using C#",
+        link: "https://dev.to/iamthecarisma/enabling-and-disabling-usb-disk-drive-using-c-4dg5",
+        site: "dev.to",
+        tags: "Csharp, .NET, Windows, Devmgmt",
+        date: "Dec 4, 2019"
+    },
+    {
+        title: "How to get your lost keystore password or alias",
+        link: "https://www.youtube.com/watch?v=4jfDwXa1h00",
+        site: "Youtube",
+        tags: "Youtube Video, Android, Java",
+        date: "Apr 20, 2017"
+    }
+]
+
+const AllProjects = [
+    {
+        name: "libcester",
+        type: "main-project",
+        description: "A robust header only unit testing framework for C programming language. Support function mocking, memory leak detection, crash report.",
+        techs: "ANSI C",
+        main_url: "https://exoticlibraries.github.io/libcester",
+        links: [
+            {
+                url: "https://github.com/exoticlibraries/libcester",
+                icon_class: "fab fa-github"
+            },
+            {
+                url: "https://exoticlibraries.github.io/libcester",
+                icon_class: "fa fa-globe"
+            }
+        ]
+    },
+    {
+        name: "objutils",
+        type: "library",
+        description: "Copy Java Object fields from multiple sources into a target with option for greater value. Ignores null fields. Most useful for model update with PATCH method + other Object Utils.",
+        techs: "#java #maven",
+        main_url: "https://github.com/thehackersdeck/objutils",
+        links: [
+            {
+                url: "https://github.com/thehackersdeck/objutils",
+                icon_class: "fab fa-github"
+            },
+            {
+                url: "https://thehackersdeck.github.io/objutils/",
+                icon_class: "fa fa-globe"
+            }
+        ]
+    },
+    {
+        name: "libmetaref",
+        type: "library",
+        description: "Reflection for C Struct. Runtime introspection and intercession for struct fields, supports struct and field annotation.",
+        techs: "#c99 #makefile",
+        main_url: "https://exoticlibraries.github.io/libmetaref/",
+        links: [
+            {
+                url: "https://github.com/exoticlibraries/libmetaref",
+                icon_class: "fab fa-github"
+            },
+            {
+                url: "https://exoticlibraries.github.io/libmetaref/",
+                icon_class: "fa fa-globe"
+            }
+        ]
+    },
+    {
+        name: "Cronux",
+        description: "Handy commands to perform simple and complex tasks with Powershell",
+        techs: "Powershell, Batch, Bash",
+        type: "main-project",
+        main_url: "https://thecarisma.github.io/Cronux/index.html",
+        links: [
+            {
+                url: "https://github.com/Thecarisma/Cronux",
+                icon_class: "fab fa-github"
+            },
+            {
+                url: "https://thecarisma.github.io/Cronux",
+                icon_class: "fa fa-globe"
+            }
+        ]
+    },
+    {
+        name: "laner",
+        description: "Fast, minimalist web framework for Java. With LAN operations, listen, detect, monitor.",
+        techs: "Java, Maven",
+        type: "main-project",
+        main_url: "https://github.com/bytesaim/laner",
+        links: [
+            {
+                url: "https://github.com/bytesaim/laner",
+                icon_class: "fab fa-github"
+            },
+            {
+                url: "https://bytesaim.github.io/laner",
+                icon_class: "fa fa-globe"
+            }
+        ]
+    },
+    {
+        name: "libxtypes",
+        description: "A library of generic, complex data types and structures for C",
+        techs: "#c #c++ #makefile",
+        type: "library",
+        main_url: "https://github.com/exoticlibraries/libxtypes",
+        links: [
+            {
+                url: "https://github.com/exoticlibraries/libxtypes",
+                icon_class: "fab fa-github"
+            },
+            {
+                url: "https://exoticlibraries.github.io/libxtypes/",
+                icon_class: "fa fa-globe"
+            }
+        ]
+    },
+    {
+        name: "themata",
+        type: "library",
+        description: "Set of Highly customizable sphinx themes. milkish, fandango, clear and fluid.",
+        techs: "#python",
+        main_url: "https://thecarisma.github.io/themata",
+        links: [
+            {
+                url: "https://github.com/Thecarisma/themata",
+                icon_class: "fab fa-github"
+            },
+            {
+                url: "https://thecarisma.github.io/themata",
+                icon_class: "fa fa-globe"
+            },
+            {
+                url: "https://pypi.org/project/themata/",
+                icon_class: "fab fa-python"
+            }
+        ]
+    },
+    {
+        name: "konfiger",
+        type: "main-project",
+        description: "Light weight package to manage key value based configuration and data files. Implemented in multiple languages.",
+        techs: "Java, Python, CSharp, ANSI C, NodeJS, Go, Elixir",
+        main_url: "https://konfiger.github.io/",
+        links: [
+            {
+                url: "https://github.com/konfiger",
+                icon_class: "fab fa-github"
+            },
+            {
+                url: "https://konfiger.github.io/",
+                icon_class: "fa fa-globe"
+            },
+            {
+                url: "https://github.com/konfiger/konfiger-c",
+                icon_class: "devicon-c-plain"
+            },
+            {
+                url: "https://www.npmjs.com/package/konfiger",
+                icon_class: "fab fa-npm"
+            },
+            {
+                url: "https://github.com/konfiger/konfiger-java",
+                icon_class: "fab fa-java"
+            },
+            {
+                url: "https://pypi.org/project/konfiger/",
+                icon_class: "fab fa-python"
+            },
+            {
+                url: "https://www.nuget.org/packages/konfiger/",
+                icon_class: "devicon-csharp-plain"
+            }
+        ]
+    },
+    {
+        name: "libnosey",
+        type: "library",
+        description: "Send system notification on various platforms.",
+        techs: "#c++ #makefile #c #bash",
+        main_url: "https://exoticlibraries.github.io/libnosey",
+        links: [
+            {
+                url: "https://github.com/exoticlibraries/libnosey",
+                icon_class: "fab fa-github"
+            },
+            {
+                url: "https://exoticlibraries.github.io/libnosey",
+                icon_class: "fa fa-globe"
+            }
+        ]
+    },
+    {
+        name: "devjammer",
+        type: "cli application",
+        description: "Regain device access if denied/disabled by other programs (esp. device control programs, ransomware)",
+        techs: "#javascript #html #css",
+        main_url: "https://thedarkprojects.github.io/devjammer/",
+        links: [
+            {
+                url: "https://github.com/Thecarisma/screenshot_it",
+                icon_class: "fab fa-github"
+            },
+            {
+                url: "https://thedarkprojects.github.io/devjammer/",
+                icon_class: "fa fa-globe"
+            }
+        ]
+    },
+    {
+        name: "language-colors",
+        type: "library",
+        description: "GitHub programming languages color codes in CSS, NodeJS and Javascript.",
+        techs: "#css #javascript #nodejs #powershell",
+        main_url: "https://www.npmjs.com/package/language-colors",
+        links: [
+            {
+                url: "https://github.com/quickutils/language-colors",
+                icon_class: "fab fa-github"
+            },
+            {
+                url: "https://quickutils.github.io/language-colors/",
+                icon_class: "fa fa-globe"
+            },
+            {
+                url: "https://quickutils.github.io/language-colors/language-colors.js",
+                icon_class: "fab fa-js"
+            },
+            {
+                url: "https://www.npmjs.com/package/language-colors",
+                icon_class: "fab fa-npm"
+            },
+            {
+                url: "https://quickutils.github.io/language-colors/language-colors.css",
+                icon_class: "fab fa-css3"
+            }
+        ]
+    },
+    {
+        name: "monitorpod.bat",
+        type: "script",
+        description: "A batch script to monitor a kubernetes pod status, it shows windows toast notification when the pod status changes. Change the NAME to your pod name.",
+        techs: "#batch",
+        main_url: "https://gist.github.com/Thecarisma/7f611cbca629c6004295b7c19ee1d4e0",
+        links: [
+            {
+                url: "https://gist.github.com/Thecarisma/7f611cbca629c6004295b7c19ee1d4e0",
+                icon_class: "fab fa-github"
+            }
+        ]
+    },
+    {
+        name: "301s",
+        type: "other",
+        description: "A static URL shortening and link management project.",
+        techs: "#javascript #html #css",
+        main_url: "https://301s.github.io/",
+        links: [
+            {
+                url: "https://github.com/301s/",
+                icon_class: "fab fa-github"
+            },
+            {
+                url: "https://301s.github.io/",
+                icon_class: "fa fa-globe"
+            }
+        ]
+    },
+    {
+        name: "audio-tube",
+        type: "library",
+        description: "A plain script and wordpress plugin to embed a youtube, vimeo and other videos as audio on your website or wordpress site",
+        techs: "#php #wordpress #javascript",
+        main_url: "https://github.com/libopenprojects/audio-tube",
+        links: [
+            {
+                url: "https://github.com/Thecarisma/audio-tube",
+                icon_class: "fab fa-github"
+            },
+            {
+                url: "https://github.com/Thecarisma/audio-tube",
+                icon_class: "fa fa-globe"
+            }
+        ]
+    },
+    {
+        name: "dec2bin",
+        type: "script",
+        description: "Convert a binary to decimal and decimal to binary (according to The Art of Assembly Language Book Chapter 1).",
+        techs: "#python",
+        main_url: "https://quickutils.github.io/dec2bin/",
+        links: [
+            {
+                url: "https://github.com/quickutils/dec2bin",
+                icon_class: "fab fa-github"
+            },
+            {
+                url: "https://quickutils.github.io/dec2bin/",
+                icon_class: "fa fa-globe"
+            }
+        ]
+    },
+    {
+        name: "stitchvariable",
+        type: "script",
+        description: "Initiate a variable name change on a Java source file, support wildcard in variable name manipulation",
+        techs: "#python",
+        main_url: "https://quickutils.github.io/stitchvariable/",
+        links: [
+            {
+                url: "https://github.com/quickutils/stitchvariable",
+                icon_class: "fab fa-github"
+            },
+            {
+                url: "https://quickutils.github.io/stitchvariable/",
+                icon_class: "fa fa-globe"
+            }
+        ]
+    },
+    {
+        name: "screenshot_it",
+        type: "bot",
+        description: "Initiate a variable name change on a Java source file, support wildcard in variable name manipulation",
+        techs: "#javascript #html #css",
+        main_url: "https://github.com/Thecarisma/screenshot_it",
+        links: [
+            {
+                url: "https://github.com/Thecarisma/screenshot_it",
+                icon_class: "fab fa-github"
+            },
+            {
+                url: "https://github.com/Thecarisma/screenshot_it",
+                icon_class: "fa fa-globe"
+            }
+        ]
+    },
+    {
+        name: "Power Text",
+        type: "desktop application",
+        description: "A simple yet intelligent text editor. Power Text support syntax highlighting for over 15 programming language. WACK",
+        techs: "#java #xml #lex",
+        main_url: "https://sourceforge.net/projects/power-text/",
+        links: [
+            {
+                url: "https://github.com/Thecarisma/powertext",
+                icon_class: "fab fa-github"
+            },
+            {
+                url: "https://sourceforge.net/projects/power-text/",
+                icon_class: "fa fa-globe"
+            }
+        ]
+    },
+    {
+        name: "Easy File Shift",
+        type: "desktop application",
+        description: "Manipulate your files the way you want, copying, moving file with different methods, hide files, view hidden file in a folder and un-hide file, search, renam, delete",
+        techs: "#java #regedit",
+        main_url: "https://sourceforge.net/projects/easy-file-shift/",
+        links: [
+            {
+                url: "https://sourceforge.net/projects/easy-file-shift/",
+                icon_class: "fa fa-globe"
+            }
+        ]
+    },
+    {
+        name: "clifilemaster.js",
+        type: "library",
+        description: "A command-line file picker for nodejs with promise support.",
+        techs: "#c++ #makefile #c #bash",
+        main_url: "https://github.com/Thecarisma/clifilemaster.js",
+        links: [
+            {
+                url: "https://github.com/Thecarisma/clifilemaster.js",
+                icon_class: "fab fa-github"
+            },
+            {
+                url: "https://github.com/Thecarisma/clifilemaster.js",
+                icon_class: "fa fa-globe"
+            }
+        ]
+    },
+    {
+        name: "async-script-loader.js",
+        type: "script",
+        description: "Load a remote javascript file in the browser synchronously. Wait till loading complete with callback.",
+        techs: "#javascript",
+        main_url: "https://gist.github.com/Thecarisma/f1fac7988b19981980752cae9f66352a",
+        links: [
+            {
+                url: "https://gist.github.com/Thecarisma/f1fac7988b19981980752cae9f66352a",
+                icon_class: "fab fa-github"
+            }
+        ]
+    },
+    {
+        name: "ptarget.h",
+        type: "script",
+        description: "header for compiler and OS detection (portable C)",
+        techs: "#c #cpp",
+        main_url: "https://gist.github.com/Thecarisma/bee1c8b94866782f634f45246ad0421d",
+        links: [
+            {
+                url: "https://gist.github.com/Thecarisma/bee1c8b94866782f634f45246ad0421d",
+                icon_class: "fab fa-github"
+            }
+        ]
+    },
+    {
+        name: "repo-page-scripts",
+        type: "web",
+        description: "Static website generator for GitHub repository, profile, and organisation using a single script. Just drop index.html and deploy.",
+        techs: "#html #css #javascript #batch",
+        main_url: "https://quickutils.github.io/repo-page-scripts/",
+        links: [
+            {
+                url: "https://github.com/quickutils/repo-page-scripts",
+                icon_class: "fab fa-github"
+            },
+            {
+                url: "https://quickutils.github.io/repo-page-scripts/",
+                icon_class: "fa fa-globe"
+            }
+        ]
+    },
+    {
+        name: "device-enabler.bat",
+        type: "script",
+        description: "Use brute-force method to enable a device when disabled by device control programs (e.g anti-virus). ",
+        techs: "#batch",
+        main_url: "https://gist.github.com/Thecarisma/b96d8cb6518a176c64b3a9d82d52ab67",
+        links: [
+            {
+                url: "https://gist.github.com/Thecarisma/b96d8cb6518a176c64b3a9d82d52ab67",
+                icon_class: "fab fa-github"
+            }
+        ]
+    }
+]
+
+
+
+
+
+
+
+
